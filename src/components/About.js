0836889d0d46fcff5934fad1e96569b779b3fb1f
zakarie <zakarie.abdi@student.moringaschool.com>
2025-07-12 11:17:12 +0300

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About ({ image="https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=" , about }){
+    return (
+        <aside>
+            <img src="image"  alt="blog logo"/>
+            <p>{about}</p>
+        </aside>    )
+}
+
+export default About;
